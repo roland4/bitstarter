f155dba4f8f7ad89044ad4240c78356fa4ee2efa
Roland Schumann <rwschumann@gmail.com>
2013-07-12 06:25:02 +0000

    try4

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
    var fs = require('fs');
-   var buffer = new Buffer(256);
+   var buffer = new Buffer("utf-8");
    fs.readFileSync("index.html", "utf-8");
     response.send buffer.toString());
 });
