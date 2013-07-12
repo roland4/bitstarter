662dd30875c6be533ae6013dd9c8fd4059768bbe
Roland Schumann <rwschumann@gmail.com>
2013-07-12 06:50:38 +0000

    try5

--- web.js
+++ web.js
@@ -6,9 +6,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
    var fs = require('fs');
-   var buffer = new Buffer("utf-8");
-   fs.readFileSync("index.html", "utf-8");
-    response.send buffer.toString());
+   var buffer = new Buffer(fs.readFileSync("index.html"), "utf-8");
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
